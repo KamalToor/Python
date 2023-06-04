f40fb409b19928b67248126e83d45ad8927ceb3c
KamalToor <kamal.toor98774@gmail.com>
2023-06-04 14:11:26 +0530

    second

---
+++ unique_character.py
@@ -0,0 +1,17 @@
+def firstUniqChar(s):
+    char_count = {}
+    
+
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+    
+
+    for i in range(len(s)):
+        if char_count[s[i]] == 1:
+            return i
+    
+    return -1
+
+print(firstUniqChar("leetcode"))     
+print(firstUniqChar("loveleetcode"))  
+print(firstUniqChar("aabb"))  
